15aec96c83e85b98b9a8143c161a98d371592111
Andrea Carrara <andreacarrara@iMac.fritz.box>
2017-03-19 14:39:13 +0100

    slave

--- slave/slave.ino
+++ slave/slave.ino
@@ -1,5 +1,6 @@
 #define pinPULSANTE 11
-
+unsigned long TdaInizioVoto,TrxSync,Tvoto;
+bool pulsantegiàpremuto;
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,6 +12,39 @@ void setup() {
 void loop() {
   if(Serial.available()) ProcessaDatiSeriali();
   ElaboraPulsante();
-  ElaboraStato();
+  ElaboraRadio();
+}
+
+void ElaboraCmdInvioSync(byte * pkt) {
+  TrxSync=micros();
+  // estrae l'informazione dal pacchetto
+  TdaInizioVoto=pkt[0];
+  pulsantegiàpremuto=false;
+  impostaled(1,1); //1 Hz Dc=10%
+  Tvoto=0;
+}
+
+void ElaboraPulsante() {
+  if(pin==0) {
+    if (!pulsantegiàpremuto) {
+      pulsantegiàpremuto=true;
+      Tvoto=micros()-TrxSync+TdaInizioVoto;
+    }
+  }
+}
+
+void ElaboraRadio() {
+  byte pkt[7];
+  if(!datidisponibili) return;
+  switch(pkt[0]) {
+    case 's':
+        ElaboraCmdInvioSync(pkt);
+    case 'p':
+        ElaboraPoll(pkt);
+    case 'd':
+        ElaboraCmdDiscovery(pkt);
+    
+        
+  }
 }
 
